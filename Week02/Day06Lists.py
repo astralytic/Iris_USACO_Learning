a22d450b0682e2602af7ba8d3385e91e8f579202
iris-ian-python-coders <115740582+iris-ian-python-coders@users.noreply.github.com>
2026-08-01 15:45:50 -0500

    Create Day06Lists.py for list examples
    Added a Python script to demonstrate list usage with family and month data.

---
+++ Week02/Day06Lists.py
@@ -0,0 +1,27 @@
+#Sat Aug 1, 2026
+
+family = [["Iris", 1.41],
+        ["Ian", 1.37],
+          ["Fang", 1.58],
+          ["Yong", 1.72]
+         ]
+
+print(family)
+
+# [['Iris', 1.41], ['Ian', 1.37], ['Fang', 1.58], ['Yong', 1.72]]
+
+months = ['January', 'February', 'March']
+prices = [238.11, 237.81, 238.91]
+
+print(months.index('February'))
+
+# 1
+
+months = ['January', 'February', 'March']
+prices = [238.11, 237.81, 238.91]
+
+print(prices[1])
+
+# 237.81
+
+
