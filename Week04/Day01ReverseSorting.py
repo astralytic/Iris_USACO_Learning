36cbad76661c57c12616e713ca079e8121466780
iris-ian-python-coders <115740582+iris-ian-python-coders@users.noreply.github.com>
2026-08-10 19:42:19 -0500

    Create Day01ReverseSorting.py for reverse sorting
    Add a script to sort numbers in reverse order.
    
    ~Iris

---
+++ Week04/Day01ReverseSorting.py
@@ -0,0 +1,16 @@
+#Mon Aug 10, 2026
+
+nums = [1, 4, 8, 6, 5, 3]
+
+numbers.sort(reverse = True)
+
+print(nums)
+
+#OR
+
+nums = [1, 4, 8, 6, 5, 3]
+
+greattoleast = sorted(nums, reverse = True)
+
+print(nums)
+print(greattoleast)
