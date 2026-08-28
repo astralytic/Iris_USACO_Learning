8c61ac2d2f9a8cb1d9d87cbf3e23e8a35d38ce9c
iris-ian-python-coders <115740582+iris-ian-python-coders@users.noreply.github.com>
2026-08-27 20:04:33 -0500

    Create RestOfTheWeek.cpp with Hello World program
    Added a simple C++ program that prints 'Hello World'.
    ~Iris

---
+++ Week05/RestOfTheWeek.cpp
@@ -0,0 +1,12 @@
+//Rest of the week:
+
+/*Watched USACO Videos: 
+and learned some basic cpp notation
+so here!*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n";
+}
